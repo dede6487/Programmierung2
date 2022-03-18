c8c5f0b4b8fc3177154fcae47eb9a6a9439ac7ee
Felix <dede4861@gmail.com>
2022-03-18 10:11:17 +0100

    Uebung1
    creating Uebung1 project, imporing main.cpp, saving exercise.pdf

---
+++ Uebung1/Uebung1/Uebung1/Main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <cstdlib>
+#include <cmath>
+
+#include "Drawing.h"
+
+#if defined(_WIN32) || defined (_WIN64)
+#include <windows.h>
+#else
+#include <time.h>
+static void Sleep(int ms)
+{
+	struct timespec ts;
+	ts.tv_sec = ms / 1000;
+	ts.tv_nsec = (ms % 1000) * 1000000;
+	nanosleep(&ts, NULL);
+}
+#endif
+
+using namespace std;
+using namespace compsys;
+
+int main(int argc, const char* argv[])
+{
+	beginDrawing(W, H, "Atoms", 0xFFFFFF, false);
+	int n = number(argc, argv);
+	Atom* atoms = new Atom[n];
+	init(n, atoms, argc, argv);
+	draw(n, atoms);
+	cout << "Press <ENTER> to continue..." << endl;
+	string s; getline(cin, s);
+	for (int i = 0; i < F; i++)
+	{
+		update(n, atoms);
+		draw(n, atoms);
+		Sleep(S);
+	}
+	delete[] atoms;
+	cout << "Close window to exit..." << endl;
+	endDrawing();
+}
