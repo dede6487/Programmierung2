cb101605c662ba969f87d8bce355c1299e7e9973
Felix_Desktop <dede6487@gmail.com>
2022-04-18 19:11:41 +0200

    working on the add functions and the copy constructor

--- Uebung2/Assignment2/Assignment2/DistPoly.cpp
+++ Uebung2/Assignment2/Assignment2/DistPoly.cpp
@@ -5,10 +5,31 @@ using namespace std;
 
 DistPoly& DistPoly::add(int coeff, int* exps) {
 
-    for (int i = 0; i <= n; i++) {
-        if (this->exps[i] == exps[i]) {
-            this->coeff = coeff;
-        }
+    //falls die polynome gleiche exponenten haben oder das erste das nullpolynom ist, können sie addiert werden
+    //if (this->n = 0) {
+    //    this->coeff = coeff;
+    //    this->exps = exps;
+    //}
+    //else {
+        for (int i = 0; i <= n; i++) {
+            if (this->exps[i] != exps[i]) {
+
+                //soll zukünftig ein um ein polynom größeres array an polynomen erstellen
+                //funktioniert zurzeit (vielleicht) nur mit länge 1
+                DistPoly p(this->n, this->vars);
+                p.coeff = coeff;
+                p.exps = exps;
+
+                DistPoly q[2] = { *this, p };
+
+                //q stimmt mit DistPoly& nicht zusammen, muss der selbe typ sein
+
+                break;
+            }
+            else if (n == i) {
+                this->coeff += coeff;
+            }
+        //}
     }
 
     return *this;
@@ -20,25 +41,34 @@ DistPoly& DistPoly::add(DistPoly& p) {
 }
 
 void DistPoly::println() {
-    cout << "[" << "n = " << n << endl;
-    cout << "[" << "vars = " << vars[1] << endl;
-    cout << "[" << "coeff = " << coeff << endl;
-    cout << "[" << "exps = " << exps[1] << endl;
+    cout << "n = " << this->n << endl;
+    for (int i = 0; i < n; i++) {
+        cout << "vars = " << this->vars[i] << endl;
+    }
+    cout << "coeff = " << this->coeff << endl;
+    for (int i = 0; i < n; i++) {
+        cout << "exps = " << this->exps[i] << endl;
+    }
 }
 
 DistPoly::DistPoly(int n, string* vars) {
     this->n = n;
     this->vars = vars;
     this->coeff = 0;
-    for (int i = 0; i <= n; i++) {
-        this->exps[n] = 0;
-    }
+    this->exps = new int[n];
+       for (int i = 0; i < n; i++) {
+            this->exps[i] = 0;
+       }
 }
 
 DistPoly::DistPoly(DistPoly& p) {
-
+    this->n = p.n;
+    this->vars = p.vars;
+    this->coeff = p.coeff;
+    this->exps = p.exps;
 }
 
 DistPoly::~DistPoly() {
-
+   //delete[] exps;
+   // delete[] vars;
 }
--- Uebung2/Assignment2/Assignment2/Main.cpp
+++ Uebung2/Assignment2/Assignment2/Main.cpp
@@ -5,15 +5,16 @@ string vars[2] = { "x","y" };
 
 DistPoly p(2, vars);
 
-void main() {
+int main() {
 
 	int e1[2] = { 1,2 };
 	int coeff = 4;
 
 	p.println();
-	p.add(coeff, e1);
-	p.println();
+	DistPoly q = p.add(coeff, e1);
+	q.println();
 
+	return 0;
 }
 
 
