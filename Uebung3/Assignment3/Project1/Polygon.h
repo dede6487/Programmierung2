e6d77f82a9cd7249e76e11e19f69e744005f4512
Felix_Desktop <dede6487@gmail.com>
2022-05-01 18:12:22 +0200

    creating the project

---
+++ Uebung3/Assignment3/Project1/Polygon.h
@@ -0,0 +1,35 @@
+#pragma once
+
+
+
+class Polygon
+{
+private:
+    int color = 0xFFFF;
+
+public:
+    // create polygon in denoted color (default black)
+    Polygon(unsigned int color = 0);
+
+    // copy constructor, copy assignment operator, destructor
+    Polygon(Polygon& p);
+    Polygon& operator=(Polygon& p);
+    virtual ~Polygon();
+
+    // create a heap-allocated copy of this polygon
+    virtual Polygon* clone();
+
+    // add point with relative coordinates (x,y) to polygon
+    void add(double x, double y);
+
+    // draws the polygon at absolute coordinates (x0,y0) scaled by factor f;
+    // thus every point (x,y) is drawn at position (x0+x*f, y0+y*f)
+    virtual void draw(double x0 = 0, double y0 = 0, double f = 1);
+};
+
+class RegularPolygon : public Polygon
+{
+public:
+    RegularPolygon(double x, double y, double r, int n,
+        double a = 0, unsigned int c = 0);
+};
