309b3b49e51298d5df2f1f52314915fb958ff440
Felix <dede4861@gmail.com>
2022-05-03 09:45:10 +0200

    working on draw()

--- Uebung3/Assignment3/Project1/Polygon.cpp
+++ Uebung3/Assignment3/Project1/Polygon.cpp
@@ -2,9 +2,11 @@
 #include"LinkedList.h"
 #include"Drawing.h"
 
+using namespace std;
+
 //constructor
 Polygon::Polygon(unsigned int color = 0) {
-	this->color = color;
+    this->color = color;
 }
 
 Polygon::Polygon(Polygon& p) {
@@ -28,5 +30,8 @@ void Polygon::add(double x, double y) {
 }
 
 void Polygon::draw(double x0 = 0, double y0 = 0, double f = 1) {
+    //beginDrawing(x0*3, y0*3, "Polygon",0xFFFF, false);
+    drawPolygon(, x0, y0, color);
 
+    //endDrawing();
 }
